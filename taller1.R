08d156c519a6e3c7907c3d2964764a683daf8c9b
linadaniels <lf.daniels@uniandes.edu.co>
2022-08-30 15:09:51 -0500

    Update taller1.R

---
+++ taller1.R
@@ -0,0 +1,7 @@
+library("rvest")
+
+url <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html"
+
+html <- read_html(url)
+
+html %>% html_elements(xpath = "//table") %>% html_table()
